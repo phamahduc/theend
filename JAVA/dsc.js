a9f1ca9aa5858882ce5237e88f422d88c01c55f7
Your Name <you@example.com>
2024-04-20 06:02:53 +0700

    Thêm mô tả về những thay đổi bạn đã thực hiện

--- JAVA/dsc.js
+++ JAVA/dsc.js
@@ -1,14 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.gallery img');
-
-    let angle = 0;
-
-    setInterval(() => {
-        angle += 1;
-        images.forEach((image, index) => {
-            const offsetX = 200 * Math.cos((angle + index * 36) * Math.PI / 180);
-            const offsetY = 200 * Math.sin((angle + index * 36) * Math.PI / 180);
-            image.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-        });
-    }, 50);
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    if (username === 'admin' && password === 'password') {
+        // Đăng nhập thành công
+        alert('Đăng nhập thành công!');
+        // Điều hướng đến trang chính
+        window.location.href = '../page.html'; // Thay 'homepage.html' bằng đường dẫn của trang chính
+    } else {
+        // Hiển thị thông báo lỗi
+        document.getElementById('error-message').innerText = 'Tên đăng nhập hoặc mật khẩu không đúng.';
+    }
 });
